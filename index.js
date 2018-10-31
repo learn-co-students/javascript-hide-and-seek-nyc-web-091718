77085fd1cdb5f30978594a1a87fc81bbdd364b94
Brennan Blair <jbrennan.blair@gmail.com>
2018-10-31 11:40:13 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+function getFirstSelector(selector) {
+return document.querySelector(selector)
+}
 
+//----------------------------------------------->
+
+function nestedTarget() {
+
+return document.querySelector('#nested .target')
+
+}
+
+//----------------------------------------------->
+
+function increaseRankBy(n) {
+
+  const ranks = document
+    .getElementById('app')
+    .querySelectorAll('ul.ranked-list li');
+
+  for (let i = 0; i < ranks.length; i++) {
+    ranks[i].innerHTML = (parseInt(ranks[i].innerHTML) + n).toString();
+  }
+  return ranks
+}
+
+//----------------------------------------------->
+
+function deepestChild() {
+
+let x = document.querySelector('#grand-node div div div div')
+
+return x
+}
