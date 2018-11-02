82954a07ec6a40c9e37566f7f90fcb02a734125b
DDarrow123 <ddarrowphl@gmail.com>
2018-11-01 21:34:48 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+function getFirstSelector(selector){
+  var el = document.querySelector(selector);
+  return el;
+}
 
+function nestedTarget(){
+  const nestedTarget = document.querySelector('#nested div.target')
+  return nestedTarget;
+}
+
+  function increaseRankBy(n){
+    // console.log(rank)
+    const rank = document
+    .getElementById('app')
+    .querySelectorAll('ul.ranked-list li');
+
+    for (let i = 0; i < rank.length; i++) {
+      rank[i].innerHTML = parseInt(rank[i].innerHTML) + n;
+    }//rank[i]= <li>1</li>
+  }//end of func
+
+
+function deepestChild(){
+let nestedDiv = document.getElementById('grand-node')//node list
+  let nestedDivChildrenNode = nestedDiv.children[0]
+  //takes divs out of 'html collection'
+
+    while (nestedDivChildrenNode) {
+      nestedDiv = nestedDivChildrenNode;
+      nestedDivChildrenNode = nestedDiv.children[0];
+    }
+
+    return nestedDiv;
+}
